c2f94898b80ca15becd039b3be073e818e5dd8cb
baragu <baraguantonyy@gmail.com>
2022-12-10 00:27:05 +0300

    Alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main block
+ * Description Get a random it with $
+ * Return: 0
+ **/
+int main(void)
+{
+	char c = 'a';
+
+	for (c = 'a'; c <= 'z';  c++)
+	{
+	putchar(c);
+	}
+
+	putchar('\n');
+	return 0;
+}
