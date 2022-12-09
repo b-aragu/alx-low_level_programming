efe594e770ed8192a388e030d48fb1ef0694f6f7
baragu <baraguantonyy@gmail.com>
2022-12-10 01:28:31 +0300

    my

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+*main - the main function
+*Return: 0
+**/
+int main(void)
+{
+int k = 0;
+for (k = 0; k < 10;k++)
+{
+	printf("%d\n",k);
+}
+return (0);
+}
