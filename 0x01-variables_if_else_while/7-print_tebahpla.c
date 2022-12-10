f6651cde12bfe29bfd6a90df5a3ca47bf9eea6d0
baragu <baraguantonyy@gmail.com>
2022-12-10 11:08:13 +0300

    reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main block
+ * Description Get a random it with $
+ * Return: 0
+ **/
+int main(void)
+{
+	char c = 'z';
+
+	for (c = 'z'; c >= 'a';  c--)
+	{
+	putchar(c);
+	}
+
+	putchar('\n');
+	return (0);
+}
