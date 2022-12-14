eecade1f15fe7fb2e3c9f04679309f10955942e7
baragu <baraguantonyy@gmail.com>
2022-12-14 22:44:00 +0300

    Alphabets

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,22 +1,22 @@
 #include "main.h"
 
 /**
-* print_sign: function that prints the sign of a number
+* print_sign - prints the sign of a number
 * @n: Number to be checked
-* Return: 1 if n is greater than zero, 0 if n is equal to zero, -1 if n is less than -1
+* Return: 1 for positive num, -1 for negative and zero for anything else
 **/
 
 int print_sign(int n)
 {
 	if (n > 0)
-	{	
+	{
 		_putchar(43);
-		return(1);
+		return (1);
 	}
 	else if (n == 0)
 	{
 		_putchar(48);
-		return(0);
+		return (0);
 	}
 	else
 	{
