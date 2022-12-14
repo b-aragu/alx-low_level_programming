bf9eb1e4788d057a91d6a1fb823e96d7a5fa5bc3
baragu <baraguantonyy@gmail.com>
2022-12-14 18:20:29 +0300

    Put char completed successfully

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+*main - The main function of the program
+*Return - Always zero for the main function
+**/
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+
+}
