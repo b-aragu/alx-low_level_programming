f182f20394b717ff4cb3b61c0d3bd731783adea3
baragu <baraguantonyy@gmail.com>
2022-12-17 18:56:41 +0300

    git commit

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,18 +1,17 @@
 #include "main.h"
+
 /**
-* print_numbers - Function that prints the numbers, from 0 to 9, followed by a new line.
-*  Return: The digits 0-9 and a newline
-**/
-
-void print_numbers(void)
+* print_most_numbers - Print numbers except 2 and 4
+*/
+void print_most_numbers(void)
 {
-	int  c;
+	char c;
 
-	c = 0;
-
-	for (c = 0; c > 9; c++)
+	for (c = '0'; c <= '9'; c++)
 	{
+		if (c != '2' && c != '4')
 		_putchar(c);
 	}
 	_putchar('\n');
 }
+
