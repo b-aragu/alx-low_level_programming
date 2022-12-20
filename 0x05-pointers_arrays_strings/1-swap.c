019e3ffa7adb0e6cbe189c2e3af52a9803f3a3f3
baragu <baraguantonyy@gmail.com>
2022-12-21 02:11:09 +0300

    swap message

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* swap_int - Swap the values of integers
+*@a: first pointer
+*@b: second pointer
+*Return: void
+*/
+void swap_int(int *a, int *b)
+{
+	int swap;
+
+	swap = *a;
+	*a = *b;
+	*b = swap;
+}
