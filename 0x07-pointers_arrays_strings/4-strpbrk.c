de85447561ed20f2e8bd9643ce385cebb41dc568
baragu <baraguantonyy@gmail.com>
2023-01-04 22:32:47 +0300

    give

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -2,7 +2,7 @@
 #include <stddef.h>
 
 /**
- * _strpbrk searches a string for any of a set of bytes
+ * _strpbrk - searches a string for any of a set of bytes
  * @s: the string to search
  * @accept: the bytes to search for
  * Return: a pointer to the byte in @s that matches one of the bytes in
