02112fba99bd162323f75ac9dcf70942345e58fc
baragu <baraguantonyy@gmail.com>
2023-01-04 15:20:56 +0300

    fix the bug

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
-* _memset - fills the first n bytes of the memory area pointed to by s with the constant byte b.
+* _memset - fills the first n bytes of the memory area pointed.
 * @s: a character.
 * @b: a character.
 * @n: an unsigned integer.
