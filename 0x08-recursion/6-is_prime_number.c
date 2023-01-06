b605e25f2aafcf07fce8ae84a1a867b282d9cb0b
baragu <baraguantonyy@gmail.com>
2023-01-06 22:27:19 +0300

    mer

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -4,7 +4,7 @@
 int check_prime(int n, int i);
 
 /**
- * is_prime_number - Returns if a number is prime
+ * is_prime_number - Return if a number is prime
  * @n: the number to be checked
  *
  * Return: integer value
