71ef98d06ac629b6dbcb82466ea3c4c35354ad92
baragu <baraguantonyy@gmail.com>
2023-01-11 10:12:34 +0300

    chnges

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,15 +2,17 @@
 #include <stdio.h>
 
 /**
-* main: The main function of the program
+* main - The main function of the 0-whatsmyname.c program
+* @argc: number of argument counts.
+* @argv: number of argument vector
 * Return: name of program
 **/
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
 	int i;
 
-	i = argc -argc;
-	printf("%s\n",argv[i]);
-	return 0;
+	i = argc - argc;
+	printf("%s\n", argv[i]);
+	return (0);
 }
