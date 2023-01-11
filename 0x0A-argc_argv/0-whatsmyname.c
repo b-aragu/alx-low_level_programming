f672d733636ea9b8ab2e1ea9320961e44aa9cb50
baragu <baraguantonyy@gmail.com>
2023-01-11 10:06:29 +0300

    am a star

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+* main: The main function of the program
+* Return: name of program
+**/
+
+int main(int argc, char* argv[])
+{
+	int i;
+
+	i = argc -argc;
+	printf("%s\n",argv[i]);
+	return 0;
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+the final stage is acceptance and well ..
