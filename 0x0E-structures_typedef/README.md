615cafd8cb7d5eabef8126a39c995c1bb9e411b9
baragu <baraguantonyy@gmail.com>
2023-01-18 18:08:37 +0300

    I have been lazy these days

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+I am Antony and I am gonna murk them all who bothers
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef TRUSTEE
+#define TRUSTEE
+
+/**
+* struct dog - defines dog details
+* @name: The dogs name
+* @age: the dogs age
+* @owner: the dogs owner
+**/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
